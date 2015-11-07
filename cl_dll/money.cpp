e44314cc69d12350d27a877fe14db8b499e2e0d1
a1batross <a1ba.omarov@gmail.com>
2015-11-08 01:21:58 +0300

    Smooth color change from red/green to yellow

--- cl_dll/money.cpp
+++ cl_dll/money.cpp
@@ -26,31 +26,54 @@ int CHudMoney::Draw(float flTime)
 	if (!(gHUD.m_iWeaponBits & (1<<(WEAPON_SUIT)) ))
 		return 1;
 
-	int r, g, b, a = max(MIN_ALPHA, m_fFade);
+	int r, g, b, alphaBalance;
+	m_fFade -= gHUD.m_flTimeDelta;
+	if( m_fFade < 0)
+	{
+		m_fFade = 0.0f;
+		m_iDelta = 0;
+	}
+	float interpolate = ( 5 - m_fFade ) / 5;
 
-	if (m_fFade > 0)
-		m_fFade -= (gHUD.m_flTimeDelta * 20);
-
-	UnpackRGB(r,g,b, RGB_YELLOWISH);
-
-	ScaleColors(r, g, b, a );
-
-	int iDollarWidth = gHUD.GetSpriteRect(m_hSprite).right - gHUD.GetSpriteRect(m_hSprite).left;
+	int iDollarWidth = gHUD.GetSpriteRect(m_HUD_dollar).right - gHUD.GetSpriteRect(m_HUD_dollar).left;
 
 	int x = ScreenWidth - iDollarWidth * 7;
 	int y = MONEY_YPOS;
 
-	SPR_Set(gHUD.GetSprite(m_hSprite), r, g, b);
-	SPR_DrawAdditive(0, x, y, &gHUD.GetSpriteRect(m_hSprite));
+	if( m_iDelta != 0 )
+	{
+		if( m_iDelta > 0 )
+		{
+			r = interpolate * ((RGB_YELLOWISH & 0xFF0000) >> 16);
+			g = (RGB_GREENISH & 0xFF00) >> 8;
+			b = (RGB_GREENISH & 0xFF);
+		}
+		else if( m_iDelta < 0)
+		{
+			r = (RGB_REDISH & 0xFF0000) >> 16;
+			g = ((RGB_REDISH & 0xFF00) >> 8) + interpolate * (((RGB_YELLOWISH & 0xFF00) >> 8) - ((RGB_REDISH & 0xFF00) >> 8));
+			b = (RGB_REDISH & 0xFF) - interpolate * (RGB_REDISH & 0xFF);
+		}
+	}
+	else UnpackRGB(r, g, b, RGB_YELLOWISH );
+
+	alphaBalance = 128 - interpolate * (128 - MIN_ALPHA);
+
+	ScaleColors( r, g, b, alphaBalance );
+
+	SPR_Set(gHUD.GetSprite(m_HUD_dollar), r, g, b);
+	SPR_DrawAdditive(0, x, y, &gHUD.GetSpriteRect(m_HUD_dollar));
 
 	gHUD.DrawHudNumber2( x + iDollarWidth, y, false, 5, m_iMoneyCount, r, g, b );
-	FillRGBA(x + iDollarWidth / 4, y + gHUD.m_iFontHeight / 4, 2, 2, r, g, b, a );
+	FillRGBA(x + iDollarWidth / 4, y + gHUD.m_iFontHeight / 4, 2, 2, r, g, b, alphaBalance );
 	return 1;
 }
 
 int CHudMoney::VidInit()
 {
-	m_hSprite = gHUD.GetSpriteIndex( "dollar" );
+	m_HUD_dollar = gHUD.GetSpriteIndex( "dollar" );
+	m_HUD_minus = gHUD.GetSpriteIndex( "minus" );
+	m_HUD_plus = gHUD.GetSpriteIndex( "plus" );
 
 	return 1;
 }
@@ -58,7 +81,9 @@ int CHudMoney::VidInit()
 int CHudMoney::MsgFunc_Money(const char *pszName, int iSize, void *pbuf)
 {
 	BEGIN_READ( pbuf, iSize );
+	int iOldCount = m_iMoneyCount;
 	m_iMoneyCount = READ_LONG();
-	m_fFade = 200.0f; //!!!
+	m_iDelta = m_iMoneyCount - iOldCount;
+	m_fFade = 5.0f; //fade for 5 seconds
 	return 1;
 }
