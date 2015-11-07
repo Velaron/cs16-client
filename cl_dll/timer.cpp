062825aee754cae063c1a7e2775c3e196a923dd2
a1batross <a1ba.omarov@gmail.com>
2015-11-08 01:25:23 +0300

    Flicker when estimated time gets to 20 seconds and lower (estimated time based!)

--- cl_dll/timer.cpp
+++ cl_dll/timer.cpp
@@ -15,6 +15,7 @@ int CHudTimer::Init()
 	HOOK_MESSAGE( RoundTime );
 	HOOK_MESSAGE( ShowTimer );
 	m_iFlags = 0;
+	m_bPanicColorChange = false;
 	gHUD.AddHudElem(this);
 	return 1;
 }
@@ -32,9 +33,27 @@ int CHudTimer::Draw( float fTime )
 
 	if (!(gHUD.m_iWeaponBits & (1<<(WEAPON_SUIT)) ))
 		return 1;
-
 	int r, g, b;
-    UnpackRGB(r,g,b, RGB_YELLOWISH);
+	// time must be positive
+	int minutes = max( 0, (int)( m_iTime + m_fStartTime - gHUD.m_flTime ) / 60);
+	int seconds = max( 0, (int)( m_iTime + m_fStartTime - gHUD.m_flTime ) - (minutes * 60));
+
+	if( minutes != 0 && seconds > 20 )
+	{
+		UnpackRGB(r,g,b, RGB_YELLOWISH );
+	}
+	else
+	{
+		m_flPanicTime += gHUD.m_flTimeDelta;
+		if( m_flPanicTime > ((float)seconds / 40.0f))
+		{
+			m_flPanicTime = 0;
+			m_bPanicColorChange = !m_bPanicColorChange;
+		}
+		UnpackRGB( r, g, b, m_bPanicColorChange ? RGB_YELLOWISH : RGB_REDISH );
+
+	}
+
     
     int iWatchWidth = gHUD.GetSpriteRect(m_HUD_timer).right - gHUD.GetSpriteRect(m_HUD_timer).left;
     
@@ -43,27 +62,19 @@ int CHudTimer::Draw( float fTime )
     
     SPR_Set(gHUD.GetSprite(m_HUD_timer), r, g, b);
     SPR_DrawAdditive(0, x, y, &gHUD.GetSpriteRect(m_HUD_timer));
-    
-    int minutes = (int)( Time + StartTime - gHUD.m_flTime ) / 60;
-    int seconds = (int)( Time + StartTime - gHUD.m_flTime ) - (minutes * 60);
-
-	if( minutes < 0 )
-		minutes = 0;
-	if( seconds < 0 )
-		seconds = 0;
-    
+        
 	x = gHUD.DrawHudNumber2( x + iWatchWidth, y, false, 2, minutes, r, g, b );
 	FillRGBA(x + iWatchWidth / 4, y + gHUD.m_iFontHeight / 4, 2, 2, r, g, b, 100);
 	FillRGBA(x + iWatchWidth / 4, y + gHUD.m_iFontHeight - gHUD.m_iFontHeight / 4, 2, 2, r, g, b, 100);
-    gHUD.DrawHudNumber2( x + iWatchWidth / 2, y, true, 2, seconds, r, g, b );
+	gHUD.DrawHudNumber2( x + iWatchWidth / 2, y, true, 2, seconds, r, g, b );
 	return 1;
 }
 
 int CHudTimer::MsgFunc_RoundTime(const char *pszName, int iSize, void *pbuf)
 {
 	BEGIN_READ( pbuf, iSize );
-	Time = READ_SHORT();
-	StartTime = gHUD.m_flTime;
+	m_iTime = READ_SHORT();
+	m_fStartTime = gHUD.m_flTime;
 	m_iFlags = HUD_ACTIVE;
 	return 1;
 }
