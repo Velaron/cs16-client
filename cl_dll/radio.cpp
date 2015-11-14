d1c62b8b1cd8d239edb15db792f3ad71f859cb28
a1batross <a1ba.omarov@gmail.com>
2015-11-14 22:09:00 +0300

    Fix broken radio

--- cl_dll/radio.cpp
+++ cl_dll/radio.cpp
@@ -12,12 +12,19 @@ int CHudRadio::Init( )
 	HOOK_MESSAGE( SendAudio );
 	HOOK_MESSAGE( ReloadSound );
 	gHUD.AddHudElem(this);
-	m_iFlags = HUD_ACTIVE;
+	m_iFlags = 0;
 	return 1;
 }
 
 int CHudRadio::Draw(float flTime)
 {
+	if( m_sentence[0] == '%' )
+		PlaySound( &m_sentence[1], m_iPitch );
+	else
+		PlaySound( m_sentence, m_iPitch );
+
+	m_iFlags = 0;
+
 	return 1;
 }
 
@@ -33,11 +40,7 @@ int CHudRadio::MsgFunc_SendAudio(const char *pszName, int iSize, void *pbuf)
 	m_iSenderID = READ_BYTE( );
 	strcpy( m_sentence, READ_STRING( ));
 	m_iPitch = READ_SHORT( );
-
-	if( m_sentence[0] == '%' )
-		PlaySound( &m_sentence[1], m_iPitch );
-	else
-		PlaySound( m_sentence, m_iPitch );
+	m_iFlags = HUD_ACTIVE;
 
 	return 1;
 }
@@ -46,10 +49,11 @@ int CHudRadio::MsgFunc_ReloadSound(const char *pszName, int iSize, void *pbuf)
 {
 	BEGIN_READ( pbuf, iSize );
 
-	int volume = READ_BYTE();
+	m_iPitch = READ_BYTE();
 	int isNotShotgun = READ_BYTE();
 
-	PlaySound( (char*)(isNotShotgun? "weapons/generic_reload.wav" : "weapons/generic_shot_reload.wav"), volume );
+	strcpy( m_sentence,  (char*)(isNotShotgun? "weapons/generic_reload.wav" : "weapons/generic_shot_reload.wav"));
+	m_iFlags = HUD_ACTIVE;
 }
 
 void Broadcast( const char *sentence )
