b0d81e9ffb246079a18f5b86834fdc95cffe016e
a1batross <a1ba.omarov@gmail.com>
2015-11-28 19:45:57 +0300

    Add studio renderer from Chicken Fortress

--- cl_dll/GameStudioModelRenderer.h
+++ cl_dll/GameStudioModelRenderer.h
@@ -1,26 +1,87 @@
-//========= Copyright © 1996-2002, Valve LLC, All rights reserved. ============
-//
-// Purpose: 
-//
-// $NoKeywords: $
-//=============================================================================
 
-#if !defined( GAMESTUDIOMODELRENDERER_H )
-#define GAMESTUDIOMODELRENDERER_H
-#if defined( _WIN32 )
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+
+// Big thanks to Chicken Fortress developers
+// for this code.
+
 #pragma once
-#endif
+#if !defined (GAMESTUDIOMODELRENDERER_H)
+#define GAMESTUDIOMODELRENDERER_H
 
-/*
-====================
-CGameStudioModelRenderer
+enum BoneIndex
+{
+	BONE_HEAD,
+	BONE_PELVIS,
+	BONE_SPINE1,
+	BONE_SPINE2,
+	BONE_SPINE3,
+	BONE_MAX,
+};
 
-====================
-*/
+typedef struct
+{
+	vec3_t origin;
+	vec3_t angles;
+
+	vec3_t realangles;
+
+	float animtime;
+	float frame;
+	int sequence;
+	int gaitsequence;
+	float framerate;
+
+	int m_fSequenceLoops;
+	int m_fSequenceFinished;
+
+	byte controller[4];
+	byte blending[2];
+
+	latchedvars_t lv;
+}client_anim_state_t;
+
 class CGameStudioModelRenderer : public CStudioModelRenderer
 {
 public:
-	CGameStudioModelRenderer( void );
+	CGameStudioModelRenderer(void);
+
+public:
+	virtual void StudioSetupBones(void);
+	virtual void StudioEstimateGait(entity_state_t *pplayer);
+	virtual void StudioProcessGait(entity_state_t *pplayer);
+	virtual int StudioDrawPlayer(int flags, entity_state_t *pplayer);
+	virtual int _StudioDrawPlayer(int flags, entity_state_t *pplayer);
+	virtual void StudioFxTransform(cl_entity_t *ent, float transform[3][4]);
+	virtual void StudioPlayerBlend(mstudioseqdesc_t *pseqdesc, int *pBlend, float *pPitch);
+	virtual void CalculateYawBlend(entity_state_t *pplayer);
+	virtual void CalculatePitchBlend(entity_state_t *pplayer);
+
+private:
+	void SavePlayerState(entity_state_t *pplayer);
+	void SetupClientAnimation(entity_state_t *pplayer);
+	void RestorePlayerState(entity_state_t *pplayer);
+	mstudioanim_t* LookupAnimation(mstudioseqdesc_t *pseqdesc, int index);
+
+private:
+	int m_nPlayerGaitSequences[MAX_CLIENTS];
+
+private:
+	bool m_bLocal;
 };
 
-#endif // GAMESTUDIOMODELRENDERER_H
+extern CGameStudioModelRenderer g_StudioRenderer;
+
+#endif
--- cl_dll/StudioModelRenderer.h
+++ cl_dll/StudioModelRenderer.h
@@ -1,190 +1,93 @@
-//========= Copyright © 1996-2002, Valve LLC, All rights reserved. ============
-//
-// Purpose: 
-//
-// $NoKeywords: $
-//=============================================================================
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
 
-#if !defined ( STUDIOMODELRENDERER_H )
-#define STUDIOMODELRENDERER_H
-#if defined( _WIN32 )
+// Big thanks to Chicken Fortress developers
+// for this code.
 #pragma once
-#endif
+#ifndef STUDIOMODELRENDERER_H
+#define STUDIOMODELRENDERER_H
 
-/*
-====================
-CStudioModelRenderer
-
-====================
-*/
 class CStudioModelRenderer
 {
 public:
-	// Construction/Destruction
-	CStudioModelRenderer( void );
-	virtual ~CStudioModelRenderer( void );
-
-	// Initialization
-	virtual void Init( void );
-
-public:  
-	// Public Interfaces
-	virtual int StudioDrawModel ( int flags );
-	virtual int StudioDrawPlayer ( int flags, struct entity_state_s *pplayer );
+	CStudioModelRenderer(void);
+	virtual ~CStudioModelRenderer(void);
 
 public:
-	// Local interfaces
-	//
-
-	// Look up animation data for sequence
-	virtual mstudioanim_t *StudioGetAnim ( model_t *m_pSubModel, mstudioseqdesc_t *pseqdesc );
-
-	// Interpolate model position and angles and set up matrices
-	virtual void StudioSetUpTransform (int trivial_accept);
-
-	// Set up model bone positions
-	virtual void StudioSetupBones ( void );	
-
-	// Find final attachment points
-	virtual void StudioCalcAttachments ( void );
-	
-	// Save bone matrices and names
-	virtual void StudioSaveBones( void );
-
-	// Merge cached bones with current bones for model
-	virtual void StudioMergeBones ( model_t *m_pSubModel );
-
-	// Determine interpolation fraction
-	virtual float StudioEstimateInterpolant( void );
-
-	// Determine current frame for rendering
-	virtual float StudioEstimateFrame ( mstudioseqdesc_t *pseqdesc );
-
-	// Apply special effects to transform matrix
-	virtual void StudioFxTransform( cl_entity_t *ent, float transform[3][4] );
-
-	// Spherical interpolation of bones
-	virtual void StudioSlerpBones ( vec4_t q1[], float pos1[][3], vec4_t q2[], float pos2[][3], float s );
-
-	// Compute bone adjustments ( bone controllers )
-	virtual void StudioCalcBoneAdj ( float dadt, float *adj, const byte *pcontroller1, const byte *pcontroller2, byte mouthopen );
-
-	// Get bone quaternions
-	virtual void StudioCalcBoneQuaterion ( int frame, float s, mstudiobone_t *pbone, mstudioanim_t *panim, float *adj, float *q );
-
-	// Get bone positions
-	virtual void StudioCalcBonePosition ( int frame, float s, mstudiobone_t *pbone, mstudioanim_t *panim, float *adj, float *pos );
-
-	// Compute rotations
-	virtual void StudioCalcRotations ( float pos[][3], vec4_t *q, mstudioseqdesc_t *pseqdesc, mstudioanim_t *panim, float f );
-
-	// Send bones and verts to renderer
-	virtual void StudioRenderModel ( void );
-
-	// Finalize rendering
-	virtual void StudioRenderFinal (void);
-	
-	// GL&D3D vs. Software renderer finishing functions
-	virtual void StudioRenderFinal_Software ( void );
-	virtual void StudioRenderFinal_Hardware ( void );
-
-	// Player specific data
-	// Determine pitch and blending amounts for players
-	virtual void StudioPlayerBlend ( mstudioseqdesc_t *pseqdesc, int *pBlend, float *pPitch );
-
-	// Estimate gait frame for player
-	virtual void StudioEstimateGait ( entity_state_t *pplayer );
-
-	// Process movement of player
-	virtual void StudioProcessGait ( entity_state_t *pplayer );
+	virtual void Init(void);
+	virtual int StudioDrawModel(int flags);
+	virtual int StudioDrawPlayer(int flags, struct entity_state_s *pplayer);
 
 public:
+	virtual mstudioanim_t *StudioGetAnim(model_t *m_pSubModel, mstudioseqdesc_t *pseqdesc);
+	virtual void StudioSetUpTransform(int trivial_accept);
+	virtual void StudioSetupBones(void);
+	virtual void StudioCalcAttachments(void);
+	virtual void StudioSaveBones(void);
+	virtual void StudioMergeBones(model_t *m_pSubModel);
+	virtual float StudioEstimateInterpolant(void);
+	virtual float StudioEstimateFrame(mstudioseqdesc_t *pseqdesc);
+	virtual void StudioFxTransform(cl_entity_t *ent, float transform[3][4]);
+	virtual void StudioSlerpBones(vec4_t q1[], float pos1[][3], vec4_t q2[], float pos2[][3], float s);
+	virtual void StudioCalcBoneAdj(float dadt, float *adj, const byte *pcontroller1, const byte *pcontroller2, byte mouthopen);
+	virtual void StudioCalcBoneQuaterion(int frame, float s, mstudiobone_t *pbone, mstudioanim_t *panim, float *adj, float *q);
+	virtual void StudioCalcBonePosition(int frame, float s, mstudiobone_t *pbone, mstudioanim_t *panim, float *adj, float *pos);
+	virtual void StudioCalcRotations(float pos[][3], vec4_t *q, mstudioseqdesc_t *pseqdesc, mstudioanim_t *panim, float f);
+	virtual void StudioRenderModel(float *lightdir);
+	virtual void StudioRenderFinal(void);
+	virtual void StudioRenderFinal_Software(void);
+	virtual void StudioRenderFinal_Hardware(void);
+	virtual void StudioPlayerBlend(mstudioseqdesc_t *pseqdesc, int *pBlend, float *pPitch);
+	virtual void StudioEstimateGait(entity_state_t *pplayer);
+	virtual void StudioProcessGait(entity_state_t *pplayer);
 
-	// Client clock
-	double			m_clTime;				
-	// Old Client clock
-	double			m_clOldTime;			
-
-	// Do interpolation?
-	int				m_fDoInterp;			
-	// Do gait estimation?
-	int				m_fGaitEstimation;		
-
-	// Current render frame #
-	int				m_nFrameCount;
-
-	// Cvars that studio model code needs to reference
-	//
-	// Use high quality models?
-	cvar_t			*m_pCvarHiModels;	
-	// Developer debug output desired?
-	cvar_t			*m_pCvarDeveloper;
-	// Draw entities bone hit boxes, etc?
-	cvar_t			*m_pCvarDrawEntities;
-	cvar_t          *m_pCvarRighthand;
-
-	// The entity which we are currently rendering.
-	cl_entity_t		*m_pCurrentEntity;		
-
-	// The model for the entity being rendered
-	model_t			*m_pRenderModel;
-
-	// Player info for current player, if drawing a player
-	player_info_t	*m_pPlayerInfo;
-
-	// The index of the player being drawn
-	int				m_nPlayerIndex;
-
-	// The player's gait movement
-	float			m_flGaitMovement;
-
-	// Pointer to header block for studio model data
-	studiohdr_t		*m_pStudioHeader;
-	
-	// Pointers to current body part and submodel
+public:
+	double m_clTime;
+	double m_clOldTime;
+	int m_fDoInterp;
+	int m_fGaitEstimation;
+	int m_nFrameCount;
+	cvar_t *m_pCvarHiModels;
+	cvar_t *m_pCvarDeveloper;
+	cvar_t *m_pCvarDrawEntities;
+	cl_entity_t *m_pCurrentEntity;
+	model_t *m_pRenderModel;
+	player_info_t *m_pPlayerInfo;
+	int m_nPlayerIndex;
+	float m_flGaitMovement;
+	studiohdr_t *m_pStudioHeader;
 	mstudiobodyparts_t *m_pBodyPart;
-	mstudiomodel_t	*m_pSubModel;
-
-	// Palette substition for top and bottom of model
-	int				m_nTopColor;			
-	int				m_nBottomColor;
-
-	//
-	// Sprite model used for drawing studio model chrome
-	model_t			*m_pChromeSprite;
-
-	// Caching
-	// Number of bones in bone cache
-	int				m_nCachedBones; 
-	// Names of cached bones
-	char			m_nCachedBoneNames[ MAXSTUDIOBONES ][ 32 ];
-	// Cached bone & light transformation matrices
-	float			m_rgCachedBoneTransform [ MAXSTUDIOBONES ][ 3 ][ 4 ];
-	float			m_rgCachedLightTransform[ MAXSTUDIOBONES ][ 3 ][ 4 ];
-
-	// Software renderer scale factors
-	float			m_fSoftwareXScale, m_fSoftwareYScale;
-
-	// Current view vectors and render origin
-	float			m_vUp[ 3 ];
-	float			m_vRight[ 3 ];
-	float			m_vNormal[ 3 ];
-
-	float			m_vRenderOrigin[ 3 ];
-	
-	// Model render counters ( from engine )
-	int				*m_pStudioModelCount;
-	int				*m_pModelsDrawn;
-
-	// Matrices
-	// Model to world transformation
-	float			(*m_protationmatrix)[ 3 ][ 4 ];	
-	// Model to view transformation
-	float			(*m_paliastransform)[ 3 ][ 4 ];	
-
-	// Concatenated bone and light transforms
-	float			(*m_pbonetransform) [ MAXSTUDIOBONES ][ 3 ][ 4 ];
-	float			(*m_plighttransform)[ MAXSTUDIOBONES ][ 3 ][ 4 ];
+	mstudiomodel_t *m_pSubModel;
+	int m_nTopColor;
+	int m_nBottomColor;
+	model_t *m_pChromeSprite;
+	int m_nCachedBones;
+	char m_nCachedBoneNames[MAXSTUDIOBONES][32];
+	float m_rgCachedBoneTransform[MAXSTUDIOBONES][3][4];
+	float m_rgCachedLightTransform[MAXSTUDIOBONES][3][4];
+	float m_fSoftwareXScale, m_fSoftwareYScale;
+	float m_vUp[3];
+	float m_vRight[3];
+	float m_vNormal[3];
+	float m_vRenderOrigin[3];
+	int *m_pStudioModelCount;
+	int *m_pModelsDrawn;
+	float (*m_protationmatrix)[3][4];
+	float (*m_paliastransform)[3][4];
+	float (*m_pbonetransform)[MAXSTUDIOBONES][3][4];
+	float (*m_plighttransform)[MAXSTUDIOBONES][3][4];
 };
 
-#endif // STUDIOMODELRENDERER_H
+#endif
