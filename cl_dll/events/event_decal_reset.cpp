b49c71bd84bdebf4478000c09c20bf004c014346
a1batross <a1ba.omarov@gmail.com>
2015-12-09 00:49:36 +0300

    Fix silly bug

--- cl_dll/events/event_decal_reset.cpp
+++ cl_dll/events/event_decal_reset.cpp
@@ -7,5 +7,5 @@ void EV_DecalReset(event_args_s *args)
 	int decalnum = floor(gEngfuncs.pfnGetCvarFloat("r_decals"));
 
 	for( int i = 0; i < decalnum; i++ )
-		gEngfuncs.pEfxAPI->R_DecalRemoveAll(1);
+		gEngfuncs.pEfxAPI->R_DecalRemoveAll(i);
 }
