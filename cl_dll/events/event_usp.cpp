a4c8d192616256765a6122e02322e09ff0732a0b
a1batross <a1ba.omarov@gmail.com>
2016-01-09 17:43:46 +0300

    Yet another event_usp.cpp fix. Now it works :D

--- cl_dll/events/event_usp.cpp
+++ cl_dll/events/event_usp.cpp
@@ -49,6 +49,8 @@ void EV_FireUSP( event_args_t *args )
 	const char *szSoundName;
 
 	idx = args->entindex;
+	bool silencer_on = !args->bparam2;
+	bool empty		 = args->bparam1;
 	VectorCopy( args->origin, origin );
 	VectorCopy( args->angles, angles );
 	VectorCopy( args->velocity, velocity );
@@ -58,36 +60,25 @@ void EV_FireUSP( event_args_t *args )
 	if ( EV_IsLocal( idx ) )
 	{
 		++g_iShotsFired;
-		EV_MuzzleFlash();
 		int seq;
-		if( !args->bparam2 ) // check for silenser
+		if( g_bHoldingShield )
 		{
-			seq = gEngfuncs.pfnRandomLong(USP_UNSIL_SHOOT1, USP_UNSIL_SHOOT3);
+			if( !empty )
+				seq = gEngfuncs.pfnRandomLong(USP_SHIELD_SHOOT1, USP_SHIELD_SHOOT2);
+			else seq = USP_SHIELD_SHOOT_EMPTY;
+		}
+		else if ( silencer_on )
+		{
+			if( !empty )
+				seq = gEngfuncs.pfnRandomLong(USP_UNSIL_SHOOT1, USP_UNSIL_SHOOT3);
+			else seq = USP_UNSIL_SHOOT_EMPTY;
 		}
 		else
 		{
-			if( g_bHoldingShield )
-			{
-				if( args->bparam1 ) // is empty
-				{
-					seq = USP_SHIELD_SHOOT_EMPTY;
-				}
-				else
-				{
-					seq = gEngfuncs.pfnRandomLong(USP_SHIELD_SHOOT1, USP_SHIELD_SHOOT2);
-				}
-			}
-			else
-			{
-				if( args->bparam1 ) // is empty
-				{
-					seq = USP_SHOOT_EMPTY;
-				}
-				else
-				{
-					seq = gEngfuncs.pfnRandomLong(USP_SHOOT1, USP_SHOOT3);
-				}
-			}
+			EV_MuzzleFlash();
+			if( !empty )
+				seq = gEngfuncs.pfnRandomLong(USP_SHOOT1, USP_SHOOT3);
+			else seq = USP_SHOOT_EMPTY;
 		}
 
 		gEngfuncs.pEventAPI->EV_WeaponAnimation(seq, 2);
@@ -97,7 +88,7 @@ void EV_FireUSP( event_args_t *args )
 	EV_GetDefaultShellInfo( args, origin, velocity, ShellVelocity, ShellOrigin, forward, -right, up, 12, -10, -7 );
 	EV_EjectBrass(ShellOrigin, ShellVelocity, angles[ YAW ], shell, TE_BOUNCE_SHELL);
 
-	if( args->bparam2 )
+	if( silencer_on )
 	{
 		szSoundName = gEngfuncs.pfnRandomLong( 0, 1 ) ? "weapons/usp1.wav" : "weapons/usp2.wav";
 	}
