f165e4a3120f8301ea4b697600248c33c88aeb85
a1batross <a1ba.omarov@gmail.com>
2016-01-11 23:39:56 +0300

    Server implementation selector. AdMob banner in LauncherActivity(haters gonna hate)

--- android/build.sh
+++ android/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-ndk-build NDK_TOOLCHAIN_VERSION=4.8 NDK_DEBUG=1 V=0 -j8 APP_CFLAGS="-w"
+ndk-build NDK_TOOLCHAIN_VERSION=4.8 NDK_DEBUG=0 V=0 -j8 APP_CFLAGS="-w"
 ant debug
-#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../myks.keystore bin/cs16-client-unsigned.apk xashdroid -tsa https://timestamp.geotrust.com/tsa
-#/home/a1ba/.android/android-sdk-linux/build-tools/22.0.1/zipalign 4 bin/cs16-client-unsigned.apk bin/cs16-client.apk
-adb install -r -f bin/cs16-client-debug.apk
+#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../myks.keystore bin/cs16-client-release-unsigned.apk xashdroid -tsa https://timestamp.geotrust.com/tsa
+#/home/a1ba/.android/android-sdk-linux/build-tools/22.0.1/zipalign 4 bin/cs16-client-release-unsigned.apk bin/cs16-client.apk
+#adb install -r -f bin/cs16-client-debug.apk
+mv bin/cs16-client-debug.apk cs16-client-debug-`date +"%d-%m-%y-%H-%M"`.apk
--- android/src/in/celest/xash3d/cs16client/LauncherActivity.java
+++ android/src/in/celest/xash3d/cs16client/LauncherActivity.java
@@ -1,98 +1,134 @@
 package in.celest.xash3d.cs16client;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
-import android.widget.CheckBox;
+import android.widget.Spinner;
 import android.widget.CompoundButton;
+import android.widget.ArrayAdapter;
 import android.content.ComponentName;
 import android.content.pm.PackageManager;
 import android.content.SharedPreferences;
 import android.util.Log;
+
+import com.google.android.gms.ads.*;
 
 import java.io.File;
 
 import in.celest.xash3d.cs16client.R;
 
 public class LauncherActivity extends Activity {
-	// public final static String ARGV = "in.celest.xash3d.MESSAGE";
 	public final static String TAG = "LauncherActivity";
+	
 	static EditText cmdArgs;
 	static SharedPreferences mPref;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launcher);
-		mPref = getSharedPreferences("mod", 0);
-		cmdArgs = (EditText)findViewById(R.id.cmdArgs);
+	static Spinner mServerSpinner;
+	static AdView mAdView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_launcher);
+		mPref          = getSharedPreferences("mod", 0);
+		cmdArgs        = (EditText)findViewById(R.id.cmdArgs);
+		mServerSpinner = (Spinner) findViewById(R.id.serverSpinner);
+		mAdView        = (AdView)  findViewById(R.id.adView);
+
 		cmdArgs.setText(mPref.getString("argv","-dev 5 -log"));
-                
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+			R.array.avail_servers, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+		mServerSpinner.setAdapter(adapter);
+		mServerSpinner.setSelection(mPref.getInt("serverSpinner", 0));
+
+		AdRequest adRequest = new AdRequest.Builder()
+			.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+			.addTestDevice("B1F9AE0E2DC2387F53BE815077840D9B")
+			.build();
+		mAdView.loadAd(adRequest);
 	}
 
-    public void startXash(View view)
-    {
+	public void startXash(View view)
+	{
+		SharedPreferences.Editor editor = mPref.edit();
+		String argv = cmdArgs.getText().toString();
+
+		editor.putString("argv", argv);
+		editor.putInt("serverSpinner", mServerSpinner.getSelectedItemPosition());
+		editor.commit();
+		editor.apply();
+
+		switch(mServerSpinner.getSelectedItemPosition())
+		{
+		case 0:
+			argv = argv + " -dll censored";
+			break;
+		case 1:
+			// Engine will load libserver.so by himself
+			break;
+		case 2:
+			String fullPath = getFilesDir().getAbsolutePath().replace("/files","/lib");
+			File yapb_hardfp = new File( fullPath + "/libyapb_hardfp.so" );
+			File yapb = new File( fullPath + "/libyapb.so" );
+			if( yapb_hardfp.exists() && !yapb_hardfp.isDirectory() )
+				argv = argv + " -dll " + yapb_hardfp.getAbsolutePath();
+			else if( yapb.exists() && !yapb.isDirectory() )
+				argv = argv + " -dll " + yapb.getAbsolutePath();
+			else
+			{
+				Log.v(TAG, "YaPB not found!");
+				AlertDialog.Builder notFoundDialogBuilder = new AlertDialog.Builder(this);
+				notFoundDialogBuilder.setMessage(R.string.not_found_msg)
+					.setTitle(R.string.not_found_title);
+				notFoundDialogBuilder.create();
+				return;
+			}
+			break;
+		case 3:
+		case 4:
+			AlertDialog.Builder notImplementedDialogBuilder = new AlertDialog.Builder(this);
+			notImplementedDialogBuilder.setMessage(R.string.not_implemented_msg)
+				.setTitle(R.string.not_implemented_title);
+			notImplementedDialogBuilder.create();
+			return;
+		}
+		
 		Intent intent = new Intent();
 		intent.setAction("in.celest.xash3d.START");
 		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-		SharedPreferences.Editor editor = mPref.edit();
-		String argv = cmdArgs.getText().toString();
-		CheckBox enableCs16nd = (CheckBox) findViewById(R.id.usecs16nd);
-		
-		editor.putString("argv", argv);
-		editor.commit();
-		editor.apply();
-		
-		if(!(enableCs16nd.isChecked()))
-                    argv = argv + " -dll censored";
-                else
-                {
-                    String fullPath = getFilesDir().getAbsolutePath().replace("/files","/lib");
-                    File yapb_hardfp = new File( fullPath + "/libyapb_hardfp.so" );
-                    File yapb = new File( fullPath + "/libyapb.so" );
-                    if( yapb_hardfp.exists() && !yapb_hardfp.isDirectory() )
-                    {
-                        argv = argv + " -dll " + yapb_hardfp.getAbsolutePath();
-                    }
-                    else
-		    {
-			Log.v(TAG, yapb_hardfp.getAbsolutePath() + "not found!");
-			if( yapb.exists() && !yapb.isDirectory() )
-                    	{
-                            argv = argv + " -dll " + yapb.getAbsolutePath();
-                    	}
-			else Log.v(TAG, yapb.getAbsolutePath() + "not found!");
-		    }
-		}
-		if(cmdArgs.length() != 0) intent.putExtra("argv", argv);
+		if(cmdArgs.length() != 0)
+			intent.putExtra("argv", argv);
 		intent.putExtra("gamedir", "cstrike");
 		intent.putExtra("gamelibdir", getFilesDir().getAbsolutePath().replace("/files","/lib"));
 		startActivity(intent);
-    }
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_launcher, menu);
-        return true;
-    }
+	@Override
+	public void onResume() {
+	super.onResume();
+	if(mAdView != null)
+		mAdView.resume();
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+	@Override
+	public void onDestroy() {
+	if(mAdView != null)
+		mAdView.destroy();
 
-        //noinspection SimplifiableIfStatement
-        /*if (id == R.id.action_settings) {
-		 return true;
-		 }*/
+	super.onDestroy();
+	}
 
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public void onPause() {
+	if(mAdView != null)
+		mAdView.pause();
+	
+	super.onPause();
+	}
 }
