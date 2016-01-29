f129a8672da2364cd7757c4858ec53e8028433ca
a1batross <a1ba.omarov@gmail.com>
2016-01-29 17:38:51 +0600

    Fix Android.mk

--- android/build.sh
+++ android/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ndk-build NDK_TOOLCHAIN_VERSION=4.8 NDK_DEBUG=0 V=0 -j8 APP_CFLAGS="-w"
+ndk-build NDK_TOOLCHAIN_VERSION=4.8 NDK_DEBUG=0 V=0 -j2
 ant debug
 #jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../myks.keystore bin/cs16-client-release-unsigned.apk xashdroid -tsa https://timestamp.geotrust.com/tsa
 #/home/a1ba/.android/android-sdk-linux/build-tools/22.0.1/zipalign 4 bin/cs16-client-release-unsigned.apk bin/cs16-client.apk
