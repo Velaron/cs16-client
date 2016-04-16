5525fe754b692d5d9eddd95b27e313d5fe2d737e
a1batross <a1ba.omarov@gmail.com>
2016-04-16 22:47:55 +0300

    release buildscript

--- android/build.sh
+++ android/build.sh
@@ -8,9 +8,11 @@ cp -vr ../3rdparty/extras/* pak/
 #cp -vr extras_src/CS16Client_vgui_team_classic/touch/* pak/touch/
 python2 makepak.py pak/ assets/extras.pak
 
-ndk-build NDK_TOOLCHAIN_VERSION=4.8 NDK_DEBUG=0 V=0 -j2
-ant debug
-#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../myks.keystore bin/cs16-client-release-unsigned.apk xashdroid -tsa https://timestamp.geotrust.com/tsa
-#/home/a1ba/.android/android-sdk-linux/build-tools/22.0.1/zipalign 4 bin/cs16-client-release-unsigned.apk bin/cs16-client.apk
+ndk-build NDK_TOOLCHAIN_VERSION=4.8 -j5
+ant release
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../myks.keystore bin/cs16-client-release-unsigned.apk xashdroid -tsa https://timestamp.geotrust.com/tsa
+/home/a1ba/.android/android-sdk-linux/build-tools/22.0.1/zipalign 4 bin/cs16-client-release-unsigned.apk bin/cs16-client.apk
 #adb install -r -f bin/cs16-client-debug.apk
 mv bin/cs16-client-debug.apk cs16-client-debug-`date +"%d-%m-%y-%H-%M"`.apk
+
+mv bin/cs16-client.apk cs16-client-release-`date +"%d-%m-%y-%H-%M"`.apk
