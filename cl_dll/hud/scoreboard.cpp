9d56e8687651b334569cbc14576af1f7ca5d6744
a1batross <a1ba.omarov@gmail.com>
2016-04-18 18:07:58 +0300

    Bring back direct CHudHealth check

--- cl_dll/hud/scoreboard.cpp
+++ cl_dll/hud/scoreboard.cpp
@@ -125,7 +125,7 @@ int CHudScoreboard :: Draw( float flTime )
 {
 	if( !m_bForceDraw )
 	{
-		if ( (!m_bShowscoresHeld && !CL_IsDead() && !gHUD.m_iIntermission) )
+		if ( (!m_bShowscoresHeld && gHUD.m_Health.m_iHealth > 0 && !gHUD.m_iIntermission) )
 			return 1;
 		else
 		{
