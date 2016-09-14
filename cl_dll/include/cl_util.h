c53ae2e0a813a74477ff44e3471bf8747b13297e
Wei Mingzhi <weimingzhi@baidu.com>
2016-09-15 00:00:29 +0800

    osx compile fix

--- cl_dll/include/cl_util.h
+++ cl_dll/include/cl_util.h
@@ -128,7 +128,9 @@ inline void PlaySound( int iSound, float vol ) { gEngfuncs.pfnPlaySoundByIndex( 
 
 #define max(a, b)  (((a) > (b)) ? (a) : (b))
 #define min(a, b)  (((a) < (b)) ? (a) : (b))
+#ifndef __APPLE__
 #define fabs(x)	   ((x) > 0 ? (x) : 0 - (x))
+#endif
 #define DotProduct(x,y) ((x)[0]*(y)[0]+(x)[1]*(y)[1]+(x)[2]*(y)[2])
 #define VectorSubtract(a,b,c) {(c)[0]=(a)[0]-(b)[0];(c)[1]=(a)[1]-(b)[1];(c)[2]=(a)[2]-(b)[2];}
 #define VectorAdd(a,b,c) {(c)[0]=(a)[0]+(b)[0];(c)[1]=(a)[1]+(b)[1];(c)[2]=(a)[2]+(b)[2];}
