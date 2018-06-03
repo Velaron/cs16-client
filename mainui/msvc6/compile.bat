66e5801a8c528f1ffd3c05dc6ba495743c9a3a3a
Alibek Omarov <a1ba.omarov@gmail.com>
2018-06-03 13:03:43 +0300

    Remove custom mainui

--- mainui/msvc6/compile.bat
+++
@@ -1,3 +0,0 @@
-..\vsmini\bin\vc
-cl /DXASH_DISABLE_FWGS_EXTENSIONS /DMY_COMPILER_SUCKS user32.lib ..\*.cpp ..\controls\*.cpp ..\menus\*.cpp ..\menus\dynamic\*.cpp -I..\ -I..\controls -I..\menus -I..\..\xash3d -I..\..\xash3d\engine -I..\..\xash3d\pm_shared -I..\..\xash3d\common -o menu.dll /link /dll /out:menu.dll
-del *.obj
