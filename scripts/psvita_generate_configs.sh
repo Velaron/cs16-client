3a0f1e5c6387420eb720887696b4022d208ca0c0
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2025-05-01 14:33:30 +0400

    scripts: enabled using special touch profile by default for PS Vita

--- scripts/psvita_generate_configs.sh
+++ scripts/psvita_generate_configs.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 touch config.cfg
-echo 'unbindall'                    >> config.cfg
-echo 'bind A_BUTTON "+use"'         >> config.cfg
-echo 'bind B_BUTTON "+jump"'        >> config.cfg
-echo 'bind X_BUTTON "+reload"'      >> config.cfg
-echo 'bind Y_BUTTON "+duck"'        >> config.cfg
-echo 'bind L1_BUTTON "+attack2"'    >> config.cfg
-echo 'bind R1_BUTTON "+attack"'     >> config.cfg
-echo 'bind START "escape"'          >> config.cfg
-echo 'bind DPAD_UP "lastinv"'       >> config.cfg
-echo 'bind DPAD_DOWN "impulse 100"' >> config.cfg
-echo 'bind DPAD_LEFT "invprev"'     >> config.cfg
-echo 'bind DPAD_RIGHT "invnext"'    >> config.cfg
-echo 'bind BACK "chooseteam"'       >> config.cfg
-echo 'gl_vsync "1"'                 >> config.cfg
-echo 'sv_autosave "0"'              >> config.cfg
+echo 'unbindall'                                     >> config.cfg
+echo 'bind A_BUTTON "+use"'                          >> config.cfg
+echo 'bind B_BUTTON "+jump"'                         >> config.cfg
+echo 'bind X_BUTTON "+reload"'                       >> config.cfg
+echo 'bind Y_BUTTON "+duck"'                         >> config.cfg
+echo 'bind L1_BUTTON "+attack2"'                     >> config.cfg
+echo 'bind R1_BUTTON "+attack"'                      >> config.cfg
+echo 'bind START "escape"'                           >> config.cfg
+echo 'bind DPAD_UP "lastinv"'                        >> config.cfg
+echo 'bind DPAD_DOWN "impulse 100"'                  >> config.cfg
+echo 'bind DPAD_LEFT "invprev"'                      >> config.cfg
+echo 'bind DPAD_RIGHT "invnext"'                     >> config.cfg
+echo 'bind BACK "chooseteam"'                        >> config.cfg
+echo 'gl_vsync "1"'                                  >> config.cfg
+echo 'sv_autosave "0"'                               >> config.cfg
+echo 'touch_config_file "touch_presets/psvita.cfg"'  >> config.cfg
 
 touch video.cfg
 echo 'fullscreen "1"' >> video.cfg
