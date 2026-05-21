de9dcc1cefe1903230564331d4ffdde5b017f99a
Alibek Omarov <a1ba.omarov@gmail.com>
2026-05-22 02:04:44 +0500

    client: demo: drop unused types, add stricter checks

--- cl_dll/include/demo.h
+++ cl_dll/include/demo.h
@@ -11,16 +11,11 @@
 // Types of demo messages we can write/parse
 enum
 {
-	TYPE_SNIPERDOT = 0,
-	TYPE_ZOOM
+	TYPE_ZOOM = 1,
 };
 
 void Demo_WriteBuffer( int type, int size, unsigned char *buffer );
 
-extern int g_demosniper;
-extern int g_demosniperdamage;
-extern float g_demosniperorg[3];
-extern float g_demosniperangles[3];
 extern float g_demozoom;
 
 #endif
